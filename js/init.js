999b7b48985f61534e0f619f2c233ee8de708e7a
Maria Jose Baeza Sanhueza <mariajosebaezasanhueza@MacBook-Air-de-Maria.local>
2016-12-02 13:23:23 -0300

    se agrega seccion mapa de Chile

--- js/init.js
+++ js/init.js
@@ -11,3 +11,36 @@
 $(document).ready(function(){
      $('.carousel').carousel();
    });
+
+
+// Mapas
+var imagenes=new Array(
+   'img/0B2.jpg',
+   'img/6EF.jpg',
+   'img/Paisaje-5.jpg'
+  
+);
+
+/**
+* Funcion para cambiar la imagen
+*/
+function rotarImagenes()
+{
+   // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+   var index=Math.floor((Math.random()*imagenes.length));
+
+   // cambiamos la imagen
+   document.getElementById("imagen").src=imagenes[index];
+}
+
+/**
+* Función que se ejecuta una vez cargada la página
+*/
+onload=function()
+{
+   // Cargamos una imagen aleatoria
+   rotarImagenes();
+
+   // Indicamos que cada 5 segundos cambie la imagen
+   setInterval(rotarImagenes,5000);
+}
